f1e036aae186fbf6998c5ccbf95a79c62f445617
Wilson Intai <wilsonintai76@gmail.com>
2026-03-05 14:18:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3599802628-88927",
+  "appId": "1:734099134013:web:f7c98c58b8f2724034bc54",
+  "apiKey": "AIzaSyATIeGTX_Y9K5DEvgv1EHfZ4OdU8NQv_N8",
+  "authDomain": "studio-3599802628-88927.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "734099134013"
+};
